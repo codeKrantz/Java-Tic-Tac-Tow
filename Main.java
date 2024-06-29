ed58bd05063937d146c40c50a20c1f2b10efa603
Conrad Krantz <conradkrantz@gmail.com>
2024-06-29 18:04:23 -0500

    added print function and constructor

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main{
     public static void main(String[] args){
-        TicTacTow test = new TicTacTow();
+        String[][] board = new String[3][3];
+
+
+
+        TicTacTow game = new TicTacTow(board);
+        game.printBoard();
     }
 }
--- TicTacTow.java
+++ TicTacTow.java
@@ -1,5 +1,52 @@
 public class TicTacTow{
-    public TicTacTow(){
-        System.out.println("test");
+    private String[][] gameBoard;
+    
+    
+    
+    public TicTacTow(String[][] gameArr){
+        gameBoard = gameArr;
+        //set all spots to Open with the letter N
+        for(int r = 0; r < gameBoard.length; r++){
+            for (int c = 0; c < gameBoard[0].length; c++){
+                gameBoard[r][c] = "N";
+            }
+    
+        }
     }
+ 
+
+
+    // function to print the board
+    public  void printBoard(){
+        for(int r = 0; r < gameBoard.length; r++){
+            System.out.println(gameBoard[r][0]+"|"+gameBoard[r][1]+"|"+gameBoard[r][2]);
+            if(r < 2){
+                System.out.println("-----");
+            }
+        }
+    }
+
+
+
+    //function to check rows
+
+
+
+    //function to check columns
+
+
+    //function to check diagonals
+
+
+
+    //master check function
+
+
+
+
+    //computer randomized function
+
+
+
+    //function to know if the board is completely filled
 }
