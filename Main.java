e78a582b9d7f44f5d3a4d163874723bf45aa8c9a
codeKrantz <conradkrantz@gmail.com>
2025-08-12 16:06:05 -0500

    First commit from new computer

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main{
     public static void main(String[] args){
         String[][] board = new String[3][3];
 
-
+        System.out.print("Hello");
         System.out.println("Welcome to Terminal TicTakTow!");
         //the player and computer letters can change in the game
         TicTacTow game = new TicTacTow(board, "X", "O");
@@ -10,3 +10,5 @@ class Main{
         game.printBoard();
     }
 }
+
+//this is a small change becasue I needed to commit on a different computer
